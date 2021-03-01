73c3157d6de8feb485d8d06bd29cdfb5da7517c9
cosul-bunicii <79849563+cosul-bunicii@users.noreply.github.com>
2021-03-01 15:33:02 +0200

    initial upload

---
+++ precache-manifest.61578e27938289dd26d75f03acef657c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afc4b585d4fc5670b97",
+    "url": "/css/app.8caa578c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b0b0e72d30817067d29d147da925d8d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0afc4b585d4fc5670b97",
+    "url": "/js/app.e3298557.js"
+  },
+  {
+    "revision": "f9b4d57ea9851ad0c9bd",
+    "url": "/js/chunk-vendors.f01202b3.js"
+  },
+  {
+    "revision": "298cc659a81f7b783326a4ce5b38e2c1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
