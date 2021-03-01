a463f92b70e8928ad12eb1927ba208b69c1076aa
cosul-bunicii <79849563+cosul-bunicii@users.noreply.github.com>
2021-03-01 15:41:44 +0200

    updated resource reference

---
+++ precache-manifest.9c30eeab97f1844a3bdf1a90efcc9670.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14a0e5687df996dc0689",
+    "url": "css/app.8caa578c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f91e2f7bfb6b98311a913c3a1f3c20c6",
+    "url": "index.html"
+  },
+  {
+    "revision": "14a0e5687df996dc0689",
+    "url": "js/app.b7bbf049.js"
+  },
+  {
+    "revision": "f9b4d57ea9851ad0c9bd",
+    "url": "js/chunk-vendors.f01202b3.js"
+  },
+  {
+    "revision": "298cc659a81f7b783326a4ce5b38e2c1",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.61578e27938289dd26d75f03acef657c.js"
+  "precache-manifest.9c30eeab97f1844a3bdf1a90efcc9670.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "cosul-bunicii"});
